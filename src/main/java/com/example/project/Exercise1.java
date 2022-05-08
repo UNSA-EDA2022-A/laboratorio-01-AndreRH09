143afff11c6aaabcf4766fe8f0b194f3f31aff3c
AndreRH09 <91095214+AndreRH09@users.noreply.github.com>
2022-05-08 12:51:20 -0500

    Update Exercise1.java
    E1

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -13,10 +13,29 @@ public class Exercise1 {
 			System.out.println(obj.convertirBaseGabriel(n));			
 		}
 	}
-
+	
+	
 	public String convertirBaseGabriel(int a) {
+		int sig = 0;
+		String fig = Integer.toString(a);
+		double result=0;
+		double aux;
 
-		// TO DO
-		return "El numero proporcionado no esta en base Gabriel.";
+		for (int i = 0; i < fig.length(); i++) {
+		    aux = fig.length()-i;
+
+		    if((sig>0 && num(fig,i) > 0) || num(fig,i)>2)
+			return "El numero proporcionado no esta en base Gabriel.";
+		    if(num(fig, i) == 2)
+			sig++;
+
+		    result = (num(fig, i)*((Math.pow(2, aux))-1))+result;
+		}
+
+		return Integer.toString((int)result);
+	    }
+	
+	public static int num(String fig,int i) {
+		return Integer.parseInt(fig.substring(i,i+1));
 	}
 }
